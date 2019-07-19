14c92bfb22b9daafb08f61f040e13457a78a653b
Laureen Cruz <laureencrz@outlook.com>
2019-07-19 16:08:49 -0700

    added migrations

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
+
 const app = require('./app')
-const { PORT } = require('./config')
+//const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
